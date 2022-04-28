37c22447515210fb8d7c9942472b869ecedb8fa7
Ashley Peterson <apeterson9@wisc.edu>
2022-04-28 17:10:49 -0500

    Fixed error

--- rename_movies.py
+++ rename_movies.py
@@ -4,19 +4,14 @@
 import os, shutil, pandas as pd, xlsxwriter
 from pathlib import Path
 
-
-# Path('my_file.mp3').suffix == '.mp3'
-
 user_input = input('Enter a directory containing movies that need to be renamed\n')
 print(f'Accessing {user_input}')
 assert os.path.exists(user_input), "I did not find the directory at, "+str(user_input)
 print("Hooray we found your directory!")
-print(os.listdir(user_input))
 
 for file in os.listdir(user_input):
-    print(file)
-    if Path(file).suffix == '.avi':
+    if file.endswith('.avi'):
         old_name = file
-        first, *last = old_name.split()
-        os.rename(old_name, last)
+        first, *last = str(old_name).split(" ")
+        os.rename(os.path.join(user_input,old_name), os.path.join(user_input,str(last[0])))
 
